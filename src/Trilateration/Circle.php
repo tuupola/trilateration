40702ae60fb021d2976281f9012158d3d4cd826a
Mika Tuupola <tuupola@appelsiini.net>
2017-05-15 12:19:58 +0700

    Add headers

--- src/Trilateration/Circle.php
+++ src/Trilateration/Circle.php
@@ -1,5 +1,18 @@
 <?php
 
+/*
+ * This file is part of trilateration package
+ *
+ * Copyright (c) 2017 Mika Tuupola
+ *
+ * Licensed under the MIT license:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *
+ * Project home:
+ *   https://github.com/tuupola/trilateration
+ *
+ */
+
 namespace Tuupola\Trilateration;
 
 use Tuupola\Trilateration;
