732b074f4dbeaffe8e3bbd48007613fcd5329ea7
Mika Tuupola <tuupola@appelsiini.net>
2017-06-22 17:26:15 +0700

    Improve coverage

--- tests/IntersectionTest.php
+++ tests/IntersectionTest.php
@@ -17,7 +17,7 @@ namespace Tuupola\Trilateration;
 
 use PHPUnit\Framework\TestCase;
 
-class NonLinearLeastSquaresTest extends TestCase
+class IntersectionTest extends TestCase
 {
     public function testShouldBeTrue()
     {
@@ -30,15 +30,15 @@ class NonLinearLeastSquaresTest extends TestCase
         $sphere2 = new Sphere(7.975332, 98.339906, 30);
         $sphere3 = new Sphere(7.975492, 98.339296, 20);
 
-        $trilateration = new NonLinearLeastSquares($sphere1, $sphere2, $sphere3);
-        $this->assertInstanceOf(NonLinearLeastSquares::class, $trilateration);
+        $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
+        $this->assertInstanceOf(Intersection::class, $trilateration);
 
         /* Closure kludge to test private properties. */
         $self = $this;
         $closure = function () use ($self) {
             $self->assertEquals(3, count($this->spheres));
         };
-        call_user_func($closure->bindTo($trilateration, NonLinearLeastSquares::class));
+        call_user_func($closure->bindTo($trilateration, Intersection::class));
     }
 
     public function testShouldAddSphere()
@@ -48,7 +48,7 @@ class NonLinearLeastSquaresTest extends TestCase
         $sphere3 = new Sphere(7.975492, 98.339296, 20);
         $sphere4 = new Sphere(7.974592, 98.339996, 25);
 
-        $trilateration = new NonLinearLeastSquares($sphere1, $sphere2, $sphere3);
+        $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
         $trilateration = $trilateration->addSphere($sphere4);
 
         /* Closure kludge to test private properties. */
@@ -56,7 +56,7 @@ class NonLinearLeastSquaresTest extends TestCase
         $closure = function () use ($self) {
             $self->assertEquals(4, count($this->spheres));
         };
-        call_user_func($closure->bindTo($trilateration, NonLinearLeastSquares::class));
+        call_user_func($closure->bindTo($trilateration, Intersection::class));
     }
 
     public function testShouldBeImmutable()
@@ -66,7 +66,7 @@ class NonLinearLeastSquaresTest extends TestCase
         $sphere3 = new Sphere(7.975492, 98.339296, 20);
         $sphere4 = new Sphere(7.974592, 98.339996, 25);
 
-        $trilateration = new NonLinearLeastSquares($sphere1, $sphere2, $sphere3);
+        $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
         $trilateration2 = $trilateration->addSphere($sphere4);
 
         /* Closure kludge to test private properties. */
@@ -74,12 +74,12 @@ class NonLinearLeastSquaresTest extends TestCase
         $closure = function () use ($self) {
             $self->assertEquals(3, count($this->spheres));
         };
-        call_user_func($closure->bindTo($trilateration, NonLinearLeastSquares::class));
+        call_user_func($closure->bindTo($trilateration, Intersection::class));
 
         $closure = function () use ($self) {
             $self->assertEquals(4, count($this->spheres));
         };
-        call_user_func($closure->bindTo($trilateration2, NonLinearLeastSquares::class));
+        call_user_func($closure->bindTo($trilateration2, Intersection::class));
     }
 
     public function testShouldFindTallinn()
@@ -87,12 +87,24 @@ class NonLinearLeastSquaresTest extends TestCase
         $sphere1 = new Sphere(60.1695, 24.9354, 82175);
         $sphere2 = new Sphere(58.3806, 26.7251, 163311);
         $sphere3 = new Sphere(58.3859, 24.4971, 117932);
-        $sphere4 = new Sphere(7.974592, 98.339996, 25);
 
-        $trilateration = new NonLinearLeastSquares($sphere1, $sphere2, $sphere3);
+        $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
         $point = $trilateration->position();
 
-        $this->assertEquals(59.436998636, round($point->latitude(), 9));
-        $this->assertEquals(24.753506837, round($point->longitude(), 9));
+        $this->assertEquals(59.412100878, round($point->latitude(), 9));
+        $this->assertEquals(24.753208418, round($point->longitude(), 9));
+    }
+
+    public function testShouldFindTallinnWithAutocorrect()
+    {
+        $sphere1 = new Sphere(60.1695, 24.9354, 81175);
+        $sphere2 = new Sphere(58.3806, 26.7251, 162311);
+        $sphere3 = new Sphere(58.3859, 24.4971, 116932);
+
+        $trilateration = new Intersection($sphere1, $sphere2, $sphere3);
+        $point = $trilateration->position();
+
+        $this->assertEquals(59.418775152, round($point->latitude(), 9));
+        $this->assertEquals(24.753287172, round($point->longitude(), 9));
     }
 }
